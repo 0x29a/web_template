42b223945aac5da412a62fe9b786d50c1793173c
0x29a <grrrr@protonmail.com>
2021-12-28 22:27:36 +0100

    One more fix for the Netlify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
         <p className="fs-5 col-md-8">Quickly and easily get started with Bootstrap's compiled, production-ready files with this barebones example featuring some basic HTML and helpful links. Download all our examples to get started.</p>
 
         <div className="mb-5">
-          <a href="/docs/5.1/examples/" className="btn btn-primary btn-lg px-4">Download examples</a>
+          <a href="/docs/5.1/examples/" className="btn btn-dark btn-lg px-4">Download examples</a>
         </div>
 
         <hr className="col-3 col-md-2 mb-5"></hr>
@@ -25,7 +25,7 @@ function App() {
             <h2>Starter projects</h2>
             <p>Ready to beyond the starter template? Check out these open source projects that you can quickly duplicate to a new GitHub repository.</p>
             <ul className="icon-list">
-              <li><a href="https://github.com/twbs/bootstrap-npm-starter" rel="noopener" target="_blank">Bootstrap npm starter</a></li>
+              <li><a href="https://github.com/twbs/bootstrap-npm-starter">Bootstrap npm starter</a></li>
               <li className="text-muted">Bootstrap Parcel starter (coming soon!)</li>
             </ul>
           </div>
