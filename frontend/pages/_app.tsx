2bc5c953e7b8f453b820eade88003c5ea7db83ed
0x29a <grrrr@protonmail.com>
2022-10-26 19:35:23 +0200

    style: add missed semicolon

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -20,7 +20,7 @@ type AppPropsWithLayout = AppProps & {
 };
 
 // Loading bar.
-NProgress.configure({ showSpinner: false })
+NProgress.configure({ showSpinner: false });
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
