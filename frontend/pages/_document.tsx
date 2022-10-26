66d1b55c74571233990f1b121bfe3d027ac5e55c
0x29a <grrrr@protonmail.com>
2022-10-26 17:15:48 +0200

    fix: change font-display, fix formatting

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,11 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800;900&display=fallback"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800;900&display=optional"
           rel="stylesheet"
         />
       </Head>
@@ -14,5 +14,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -4,9 +4,7 @@ const About = () => {
   return (
     <div className="mt-20 max-w-6xl mx-auto px-4 sm:px-6">
       <p>About page</p>
-      <Link href="/">
-        See the homepage.
-      </Link>
+      <Link href="/">See the homepage.</Link>
     </div>
   );
 };
