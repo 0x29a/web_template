90cc9f70281cd9698ede0cdf5bfcee0ddb3d4b01
0x29a <grrrr@protonmail.com>
2022-11-08 13:06:24 +0100

    feat: finish desktop header

---
+++ frontend/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,23 @@
+import type { ReactElement } from 'react'
+
+function Dropdown({ children, title }: { children: ReactElement[], title: string }) {
+    return (
+        <li className="relative group">
+            <a
+                className="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center"
+                href="#0"
+                onClick={(e) => e.preventDefault()}
+            >
+                {title}
+                <svg className="w-3 h-3 fill-current text-gray-500 cursor-pointer ml-1 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.28 4.305L5.989 8.598 1.695 4.305A1 1 0 00.28 5.72l5 5a1 1 0 001.414 0l5-5a1 1 0 10-1.414-1.414z" />
+                </svg>
+            </a>
+            <ul className="absolute top-full right-0 w-40 bg-white py-2 ml-4 rounded shadow-lg hidden group-hover:block">
+                {children}
+            </ul>
+        </li>
+    )
+};
+
+export default Dropdown;
--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 
 import Link from "next/link";
+import Dropdown from '../Dropdown/Dropdown';
 
 export default function Header() {
     const [mobileNavOpen, setMobileNavOpen] = useState(false);
@@ -76,9 +77,33 @@ export default function Header() {
                             <li>
                                 <Link href="/" className="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center">Blog</Link>
                             </li>
+                            <Dropdown title="Resources">
+                                <li>
+                                    <Link href="/documentation" className="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight">Documentation</Link>
+                                </li>
+                                <li>
+                                    <Link href="/support" className="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight">Support center</Link>
+                                </li>
+                                <li>
+                                    <Link href="/404" className="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight">404</Link>
+                                </li>
+                            </Dropdown>
                         </ul>
+
                         {/* Desktop sign in links */}
-                        <ul></ul>
+                        <ul className="flex grow justify-end flex-wrap items-center">
+                            <li>
+                                <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
+                            </li>
+                            <li>
+                                <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                                    <span>Sign up</span>
+                                    <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
+                                    </svg>
+                                </Link>
+                            </li>
+                        </ul>
                     </nav>
                 </div>
             </div>
