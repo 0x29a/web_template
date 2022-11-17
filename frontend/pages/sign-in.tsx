6ec577dc8aa4c779ad0413b0cbfad7f7c5db877c
0x29a <grrrr@protonmail.com>
2022-11-17 12:20:20 +0100

    chore: test the bundle size

--- frontend/pages/sign-in.tsx
+++ frontend/pages/sign-in.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 
+import { useForm } from "react-hook-form";
+
 const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
     <div className="mt-20 max-w-6xl mx-auto px-4 sm:px-6">
       <p>Sign-in</p>
@@ -9,6 +17,14 @@ const SignIn = () => {
         <button className="btn">Try Sign In</button>
         <button className="btn">Execute the lazy query</button>
       </div>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <input {...register("firstName")} />
+        <input {...register("lastName", { required: true })} />
+        {errors.lastName && <p>Last name is required.</p>}
+        <input {...register("age", { pattern: /\d+/ })} />
+        {errors.age && <p>Please enter number for age.</p>}
+        <input type="submit" />
+      </form>
     </div>
   );
 };
